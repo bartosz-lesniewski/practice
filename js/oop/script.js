821f53067c8985cb1da5e1fa6c557db9a70eab2d
bartosz-lesniewski <b.lesniewski8@gmail.com>
2021-03-09 17:29:23 +0100

    task 1 code changed into oop

--- js/oop/script.js
+++ js/oop/script.js
@@ -1,16 +1,48 @@
-let userArray = [];
+const usersArray = [];
 
-const createUser = document.querySelector('.createUser').addEventListener('click', function(user) {
-    user.name = prompt('what is your name?');
-    user.age = prompt('what is your age?');
+function createUser(name, age) {
+    const user = {}
+    user.name = name;
+    user.age = age;
+    
+    return user
+}
 
-    userArray.push(user);
+function showUser() {
+    usersArray.forEach((user) => console.log(user))
+}
+
+const clearConsole = () => console.clear()
+
+document.addEventListener('DOMContentLoaded', () =>{
+    const btnCreate = document.querySelector('.createUser');
+    const btnShow = document.querySelector('.showUser');
+    const btnClear = document.querySelector('.clearConsole');
+
+    btnCreate.addEventListener('click', () => {
+        const name = window.prompt('what is your name?');
+        const age = window.prompt('what is your age?');
+
+        usersArray.push(createUser(name, age))
+    });
+    
+    btnShow.addEventListener('click', showUser);
+    
+    btnClear.addEventListener('click', clearConsole)
 });
-const showUser = document.querySelector('.showUser').addEventListener('click', () => {
-    userArray.forEach((user) => console.log(user))
-});
-const clearConsole = document.querySelector('.clearConsole').addEventListener('click', function() {
-    console.clear()
-});
 
 
+
+
+// const createUser = document.querySelector('.createUser').addEventListener('click', function(user) {
+//     user.name = prompt('what is your name?');
+//     user.age = prompt('what is your age?');
+
+//     userArray.push(user);
+// });
+// const showUser = document.querySelector('.showUser').addEventListener('click', () => {
+//     userArray.forEach((user) => console.log(user))
+// });
+// const clearConsole = document.querySelector('.clearConsole').addEventListener('click', function() {
+//     console.clear()
+// });
